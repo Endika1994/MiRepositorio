68e09d6618c826da803247e8d83c0cfb480348bf
Curso <Curso@PORT-Z12.ipartekaula.com>
2018-11-27 09:03:52 +0100

    Proyecto por capas:usuario

--- javase/src/pojos/Usuario.java
+++ javase/src/pojos/Usuario.java
@@ -1,5 +1,23 @@
 package pojos;
 
 public class Usuario {
+	
+	private Long id;
+	private String email,password;
+	
+	public Usuario(Long id, String email, String password) {
+		super();
+		this.id = id;
+		this.email = email;
+		this.password = password;
+	}
+
+	@Override
+	public String toString() {
+		return "Usuario [id=" + id + ", email=" + email + ", password=" + password + "]";
+	}
+	
+	
+	
 
 }
